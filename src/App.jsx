69be950ca63b7ef14fada374d12121c84123c15f
Rohit Singh <jerryy1803@gmail.com>
2024-02-23 12:16:59 +0530

    Todo heading created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import React from 'react'
+import TodoHeading from './components/title'
 
 function App() {
 
   return (
     <>
-      
+      <TodoHeading />
     </>
   )
 }
---
+++ src/Components/Title.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodoHeading = () => {
+  return <h1 className="TodoHeading">#todo</h1>;
+};
+
+export default TodoHeading;
+//title.jsx
+
