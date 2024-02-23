9f4c5380764030e62e5908011fb4fbc8dec29a27
Rohit Singh <jerryy1803@gmail.com>
2024-02-23 12:18:58 +0530

    Todo input created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import React from 'react'
 import TodoHeading from './components/title'
+import TodoInput from './components/TodoInput'
 
 function App() {
 
   return (
     <>
       <TodoHeading />
+      <TodoInput />
     </>
   )
 }
 
 export default App
+
---
+++ src/Components/TodoInput.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoInput = () => {
+  return (
+    <>
+      <div className="todo-input">
+        <input type="text" placeholder="add details" />
+        <button className="btn">Add</button>
+      </div>
+    </>
+  );
+};
+
+export default TodoInput;
+//todoinput.jsx
