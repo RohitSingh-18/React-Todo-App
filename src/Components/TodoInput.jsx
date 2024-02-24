3c1a02527adf855b5af8937a5ab5353f744986ad
Rohit Singh <jerryy1803@gmail.com>
2024-02-24 16:05:00 +0530

    todo listing functionality added

--- src/Components/TodoInput.jsx
+++ src/Components/TodoInput.jsx
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoInput = () => {
+  const [Title, setTitle] = useState("");
+  const [MainTask, setMainTask] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setMainTask([...MainTask, { Title }]);
+    setTitle("");
+  };
+
+  const deleteHandler = (id) => {
+    let copyTask = [...MainTask];
+    copyTask.splice(id, 1);
+    setMainTask(copyTask);
+  };
+
+  let renderTask = (
+    <h2
+      style={{ display: "flex", justifyContent: "center", marginTop: "25px" }}
+    >
+      No Tasks
+    </h2>
+  );
+
+  if (MainTask.length > 0) {
+    renderTask = MainTask.map((task, id) => {
+      return (
+        <>
+          <li key={id}>
+            <div>
+              <h5>{task.Title}</h5>
+            </div>
+            <button
+              onClick={() => {
+                deleteHandler(id);
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        </>
+      );
+    });
+  }
+
   return (
     <>
-      <div className="todo-input">
-        <input type="text" placeholder="add details" />
+      <form onSubmit={submitHandler} className="todo-input">
+        <input
+          type="text"
+          placeholder="add details"
+          value={Title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
         <button className="btn">Add</button>
+      </form>
+
+      <div>
+        <ul>{renderTask}</ul>
       </div>
     </>
   );
